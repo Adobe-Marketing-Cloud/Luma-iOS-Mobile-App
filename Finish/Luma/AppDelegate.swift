7bfbeadffd98af67276730b226a9741254deb992
Rob In der Maur <rmaur@adobe.com>
2025-09-08 09:55:55 +0200

    Updates - to conform to new updateProposition API's - to conform to updated handleNotificationResponse - smaller updates

--- Finish/Luma/AppDelegate.swift
+++ Finish/Luma/AppDelegate.swift
@@ -207,14 +207,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Logger.notifications.info("AppDelegate - userNotificationCenter")
         switch response.actionIdentifier {
         case "ACCEPT_ACTION":
-            Messaging.handleNotificationResponse(response, applicationOpened: true, customActionId: "ACCEPT_ACTION")
+            Messaging.handleNotificationResponse(response)
 
         case "DECLINE_ACTION":
-            Messaging.handleNotificationResponse(response, applicationOpened: false, customActionId: "DECLINE_ACTION")
+            Messaging.handleNotificationResponse(response)
 
         // Handle other actions…
         default:
-            Messaging.handleNotificationResponse(response, applicationOpened: true, customActionId: nil)
+            Messaging.handleNotificationResponse(response)
         }
 
         let userInfo = response.notification.request.content.userInfo
